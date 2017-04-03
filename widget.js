bb465c9d4c1ad8e74189e849c7dcc535ad7f9b12
Przemysław <przemyslaw@MBP-Przemysaw.home>
2017-04-03 10:47:50 +0200

    weight reduce

--- widget.js
+++ widget.js
@@ -6,7 +6,7 @@
 const DEBUG = false;
 const CSS = "";
 // const ICON_BASE_64 = import from
-const ICON_BASE_64 = (function() {
+const ICON = (function() {
     return '<?xml version="1.0" encoding="iso-8859-1"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 356.484 356.484" style="enable-background:new 0 0 356.484 356.484;" xml:space="preserve"><g><g><path d="M293.984,7.23H62.5C28.037,7.23,0,35.268,0,69.731v142.78c0,34.463,28.037,62.5,62.5,62.5l147.443,0.001l70.581,70.58c2.392,2.393,5.588,3.662,8.842,3.662c1.61,0,3.234-0.312,4.78-0.953c4.671-1.934,7.717-6.49,7.717-11.547v-62.237c30.759-3.885,54.621-30.211,54.621-62.006V69.731C356.484,35.268,328.447,7.23,293.984,7.23z M331.484,212.512c0,20.678-16.822,37.5-37.5,37.5h-4.621c-6.903,0-12.5,5.598-12.5,12.5v44.064l-52.903-52.903c-2.344-2.345-5.522-3.661-8.839-3.661H62.5c-20.678,0-37.5-16.822-37.5-37.5V69.732c0-20.678,16.822-37.5,37.5-37.5h231.484c20.678,0,37.5,16.822,37.5,37.5V212.512z" /><path d="M270.242,95.743h-184c-6.903,0-12.5,5.596-12.5,12.5c0,6.903,5.597,12.5,12.5,12.5h184c6.903,0,12.5-5.597,12.5-12.5C282.742,101.339,277.146,95.743,270.242,95.743z" /><path d="M270.242,165.743h-184c-6.903,0-12.5,5.596-12.5,12.5s5.597,12.5,12.5,12.5h184c6.903,0,12.5-5.597,12.5-12.5S277.146,165.743,270.242,165.743z" /></g></g></svg>'
 })()
 
@@ -15,7 +15,7 @@ const PERSON_ID = $("meta[name=person_id]").attr('content');
 const COMPANY_TAG = $("meta[name=company_tag]").attr('content');
 const BASE_PATH = `http://news.databridgecmms.com/api/news/${COMPANY_TAG}/${PERSON_ID}`;
 
-
+// generate html base on news, if checkForNewsOnly = true then refresh list without building html
 const renderWidget = function(news, checkForNewsOnly) {
     const renderStyles = function() {
             var _style = $(`<style type="text/css">${CSS}</style>`)
@@ -24,7 +24,7 @@ const renderWidget = function(news, checkForNewsOnly) {
         // news list fixed wrapper
     if (news.length > 0) {
         if (checkForNewsOnly) {
-            var _wrapper = $('.widget-wrapper');
+            var _wrapper = $('.wwr');
             // remove existing list
             _wrapper.find('ul').remove();
             _wrapper.append('<ul></ul>');
@@ -35,42 +35,41 @@ const renderWidget = function(news, checkForNewsOnly) {
             })
 
             // add new status to get styles
-            _wrapper.addClass('new');
+            _wrapper.addClass('n');
             setTimeout(function() {
-                _wrapper.removeClass('new')
+                _wrapper.removeClass('n')
             }, 10000)
         } else {
 
-            $('.widget-wrapper').remove();
+            $('.wwr').remove();
 
             // add styles
             renderStyles();
 
             // make wrapper
-            var _wrapper = $('<div></div>').addClass('widget-wrapper loading');
+            var _wrapper = $('<div></div>').addClass('wwr l');
             // make link to crm
-            var _toCMS = $('<div class="widget-link"><a href="http://news.databridgecmms.com/"> Read <span class="hidden">New</span> News &rarr;</a></div>');
+            var _toCMS = $('<div class="wl"><a href="http://news.databridgecmms.com/"> Read <span class="hidden">New</span> News &rarr;</a></div>');
             // make toggler
-            var _toggler = $(`<div class="widget-toggler"><a>${ICON_BASE_64}</a></div>`);
+            var _toggler = $(`<div class="wt"><a>${ICON}</a></div>`);
 
 
 
             _wrapper.append(_toggler);
-
             _wrapper.append(_toCMS);
             _wrapper.append('<ul></ul>');
 
             news.map(function(elem, key) {
                 _wrapper.find('ul').append(`<li><a href="${elem.url}"><span>${key+1}.</span> ${elem.title}</a></div>`)
             })
- 
+
             // add wrapper to DOM
             $('body').append(_wrapper);
 
             //events
             // toggle open wrapper
-            $('.widget-toggler').on('click', function(e) {
-                _wrapper.toggleClass('open');
+            $('.wt').on('click', function(e) {
+                _wrapper.toggleClass('o');
                 e.preventDefault();
                 e.stopPropagation();
             })
@@ -83,7 +82,7 @@ const renderWidget = function(news, checkForNewsOnly) {
                 if (!_wrapper.is(e.target) // if the target of the click isn't the container...
                     && _wrapper.has(e.target).length === 0) // ... nor a descendant of the container
                 {
-                    _wrapper.removeClass('open');
+                    _wrapper.removeClass('o');
                 } else {
 
                 }
@@ -91,7 +90,7 @@ const renderWidget = function(news, checkForNewsOnly) {
 
             // stop loading state of widget
             setTimeout(function() {
-                _wrapper.removeClass('loading')
+                _wrapper.removeClass('l')
             }, 1500)
         }
     }
@@ -102,7 +101,7 @@ var ajaxRequest = function() {
     var time_storage = JSON.parse(localStorage.getItem('widget.time_storage')) || 0;
     var news = JSON.parse(localStorage.getItem('widget.news')) || [];
     const time_diff = 1000 * 60 * 20; // 1000 * 1000 * 60 * 20;
-    console.log('cache', time_storage, news);
+    // console.log('cache', time_storage, news);
 
     if ((Date.now() - time_diff) > time_storage) {
         // if now is 20 min after saved time get ajax
